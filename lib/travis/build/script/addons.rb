153f925480e6d7f16fbfc1cc037a0910f5872e91
Travis <dan@travis-ci.org>
2014-10-29 20:46:00 -0400

    Adding ssh known hosts addon
    so that folks can whitelist their own hostnames or ip addresses in
    order to allow for fetching dependencies over ssh from more than
    just github.com, gist.github.com, and ssh.github.com
    
    Conflicts:
    	lib/travis/build/script.rb
    	lib/travis/build/script/addons.rb

---
+++ lib/travis/build/script/addons.rb
@@ -0,0 +1,57 @@
+require 'travis/build/script/addons/artifacts'
+require 'travis/build/script/addons/code_climate'
+require 'travis/build/script/addons/coverity_scan'
+require 'travis/build/script/addons/deploy'
+require 'travis/build/script/addons/firefox'
+require 'travis/build/script/addons/hosts'
+require 'travis/build/script/addons/postgresql'
+require 'travis/build/script/addons/sauce_connect'
+require 'travis/build/script/addons/ssh_known_hosts'
+
+module Travis
+  module Build
+    class Script
+      module Addons
+        MAP = {
+          artifacts:       Artifacts,
+          code_climate:    CodeClimate,
+          coverity_scan:   CoverityScan,
+          deploy:          Deploy::Group,
+          firefox:         Firefox,
+          hosts:           Hosts,
+          postgresql:      Postgresql,
+          sauce_connect:   SauceConnect,
+          ssh_known_hosts: SshKnownHosts,
+        }
+
+        def run_addons(stage)
+          addons.each do |addon|
+            addon.send(stage) if can_run?(addon, stage)
+          end
+        end
+
+        def addons
+          @addons ||= (config[:addons] || {}).map do |name, addon_config|
+            init_addon(name, addon_config)
+          end.compact
+        end
+
+        def init_addon(name, config)
+          MAP[name] && MAP[name].new(self, config)
+        end
+
+        def can_run?(addon, stage)
+          return false if !addon.respond_to?(stage)
+
+          if !data.paranoid_mode?
+            true
+          elsif data.paranoid_mode? && addon.class::SUPER_USER_SAFE
+            true
+          else
+            false
+          end
+        end
+      end
+    end
+  end
+end
